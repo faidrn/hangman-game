4f0103e36c1d05ee82130cd1d9184d1a60fbca62
Fredy Izquierdo <m3talmaniaco@gmail.com>
2022-08-16 23:06:21 -0500

    Trying to show the hangman if select the wront letter without success

--- assets/js/logic_game.js
+++ assets/js/logic_game.js
@@ -2,6 +2,12 @@
 const printWord = document.getElementById('printWord');
 const getNewWord = document.getElementById('get-new-word');
 const letterAlphabet = document.querySelectorAll('.btn-alphabet');
+const hangmanHead = document.querySelector('.hangman-head');
+const hangmanBody = document.querySelector('.hangman-body');
+const hangmanLeftArm = document.querySelector('.hangman-left-arm');
+const hangmanRightArm = document.querySelector('.hangman-right-arm');
+const hangmanRightLeg = document.querySelector('.hangman-right-leg');
+const hangmanLeftLeg = document.querySelector('.hangman-left-leg');
 
 
 class ReadFile{
@@ -36,7 +42,12 @@ class LogicGame{
     constructor(listOfWords){
         this.listOfWords = listOfWords;
         this.word;
-        // this.underlines;
+        this.head = 0;
+        this.body = 0;
+        this.leftArm = 0;
+        this.rightArm = 0;
+        this.leftLeg = 0;
+        this.rightLeg = 0;
     }
 
     getRandomPosition(){
@@ -65,24 +76,76 @@ class LogicGame{
         return underlines;
     }
 
+    // Metodo para comparar la letra seleccionada con las letras de la palabra
     compareLetters(letter){
         // Asignar el valor actual de la cadena, para no perder las letras q se han identificado
         // Convirtiendo la cadena en array
         let underlinesArray = printWord.innerHTML.split(' ');
+        let aux = 0;
         
         // Ubicar la letra en el espacio que corresponde
         for(var i = 0; i < this.word.length; i++){
             if (this.word[i] == letter){
                 underlinesArray[i] = letter;
-            } 
+                aux -= 1;
+            } else{
+                aux += 1;
+            }
         }
         
+        if (aux == this.word.length){
+            this.drawingHangman();
+        }
+
         // Convertir el array en string
         underlinesArray = underlinesArray.toString();
         // Eliminar las comas del string
         underlinesArray = underlinesArray.replace(/,/g, " ");
 
         return underlinesArray;
+    }
+
+    // Metodo para mostrar el ahorcado
+    drawingHangman(){
+        if (this.head == 0){
+            this.head = 1;
+            hangmanHead.classList.remove('hide');
+        }
+
+        if (this.head == 1){
+            if (this.body == 0){
+                this.body = 1;
+                hangmanBody.classList.remove('hide');
+            }
+
+            if (this.body == 1){
+                if (this.rightArm == 0){
+                    this.rightArm = 1;
+                    hangmanRightArm.classList.remove('hide');
+                }
+
+                if (this.rightArm == 1){
+                    if (this.leftArm == 0){
+                        this.leftArm = 1;
+                        hangmanLeftArm.classList.remove('hide');
+                    }
+
+                    if (this.leftArm == 1){
+                        if (this.rightLeg == 0){
+                            this.rightLeg = 1;
+                            hangmanRightLeg.classList.remove('hide');
+                        }
+
+                        if (this.rightLeg == 1){
+                            if (this.leftLeg == 0){
+                                this.leftLeg = 1;
+                                hangmanLeftLeg.classList.remove('hide');
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -120,8 +183,6 @@ letterAlphabet.forEach((everyLetter, i) => {
 });
 
 
-// Comparar la letra seleccionada con las letras de la palabra
-console.log(logicGame.word);
 
 
 // read file using promises and asyn
